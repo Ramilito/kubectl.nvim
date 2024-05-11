b3ac7799206275ef40f6ea367af361522b9a0a13
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-11 15:28:30 +0200

    change log filetype to use less

--- ftplugin/k8s_logs.lua
+++ ftplugin/k8s_logs.lua
@@ -1,6 +1,7 @@
 -- k8s_logs.lua in ~/.config/nvim/ftplugin
-vim.bo.filetype = "json"
+vim.bo.filetype = "less"
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
+vim.opt_local.spell = false
 vim.cmd("syntax enable")
