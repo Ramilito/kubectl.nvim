9e05732476b9a4f19a92c5ccc76561728e5ee56d
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-22 14:58:48 +0200

    remove unused filetypes

--- ftplugin/k8s_logs.lua
+++
@@ -1,7 +0,0 @@
--- k8s_logs.lua in ~/.config/nvim/ftplugin
-vim.bo.filetype = "less"
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = true
-vim.opt_local.spell = false
-vim.cmd("syntax enable")
--- ftplugin/k8s_pod_desc.lua
+++
@@ -1,7 +0,0 @@
--- k8s_pod_containers.lua in ~/.config/nvim/ftplugin
-
-vim.bo.filetype = "yaml"
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = true
-vim.cmd("syntax enable")
