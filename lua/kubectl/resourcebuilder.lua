d3af9cda4e434ad88954c9be4175df729c910fdb
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-30 02:28:58 +0200

    using builder pattern

---
+++ lua/kubectl/resourcebuilder.lua
@@ -0,0 +1,68 @@
+local tables = require("kubectl.utils.tables")
+local find = require("kubectl.utils.find")
+local actions = require("kubectl.actions.actions")
+local commands = require("kubectl.actions.commands")
+
+local ResourceBuilder = {}
+ResourceBuilder.__index = ResourceBuilder
+
+function ResourceBuilder:new(resource, args)
+  local self = setmetatable({}, ResourceBuilder)
+  self.resource = resource
+  self.args = args
+  self.hints = {}
+  self.filter = ""
+  return self
+end
+
+function ResourceBuilder:fetch()
+  self.data = commands.execute_shell_command("kubectl", self.args)
+  return self
+end
+
+function ResourceBuilder:decodeJson()
+  local success, decodedData = pcall(vim.json.decode, self.data)
+  if success then
+    self.data = decodedData
+  end
+  return self
+end
+
+function ResourceBuilder:process(processFunc)
+  self.processedData = processFunc(self.data)
+  return self
+end
+
+function ResourceBuilder:splitData()
+  if type(self.data) == "string" then
+    self.data = vim.split(self.data, "\n")
+  end
+  return self
+end
+
+function ResourceBuilder:prettyPrint(headersFunc)
+  self.prettyData = tables.pretty_print(self.processedData, headersFunc())
+  return self
+end
+
+function ResourceBuilder:addHints(hints, include_defaults, include_context)
+  self.hints = tables.generateHints(hints, include_defaults, include_context)
+  return self
+end
+
+function ResourceBuilder:setFilter(filter)
+  self.filter = filter
+  return self
+end
+
+function ResourceBuilder:display(bufferName)
+  actions.buffer(find.filter_line(self.prettyData, self.filter), bufferName, { hints = self.hints })
+end
+
+function ResourceBuilder:displayFloat(bufferName, title, syntax)
+  print(vim.inspect(self.data))
+  actions.floating_buffer(self.prettyData, bufferName, { title = title, syntax = syntax, hints = self.hints })
+  return self
+end
+
+return ResourceBuilder
--- lua/kubectl/views/pods/views.lua
+++ lua/kubectl/views/pods/views.lua
@@ -1,39 +1,43 @@
-local actions = require("kubectl.actions.actions")
+local ResourceBuilder = require("kubectl.resourcebuilder")
+
 local commands = require("kubectl.actions.commands")
-local find = require("kubectl.utils.find")
 local pods = require("kubectl.views.pods")
-local tables = require("kubectl.utils.tables")
 
 local M = {}
 local selection = {}
 
 function M.Pods()
-  local results = commands.execute_shell_command("kubectl", { "get", "pods", "-A", "-o=json" })
-  local data = pods.processRow(vim.json.decode(results))
-  local pretty = tables.pretty_print(data, pods.getHeaders())
-
-  local hints = tables.generateHints({
-    { key = "<l>", desc = "logs" },
-    { key = "<d>", desc = "desc" },
-    { key = "<t>", desc = "top" },
-    { key = "<enter>", desc = "containers" },
-  }, true, true)
-  actions.buffer(find.filter_line(pretty, FILTER), "k8s_pods", { hints = hints })
+  ResourceBuilder:new("pods", { "get", "pods", "-A", "-o=json" })
+    :fetch()
+    :decodeJson()
+    :process(pods.processRow)
+    :prettyPrint(pods.getHeaders)
+    :addHints({
+      { key = "<l>", desc = "logs" },
+      { key = "<d>", desc = "desc" },
+      { key = "<t>", desc = "top" },
+      { key = "<enter>", desc = "containers" },
+    }, true, true)
+    :setFilter(FILTER)
+    :display("k8s_pods")
 end
 
 function M.PodTop()
-  local results = commands.execute_shell_command("kubectl", { "top", "pods", "-A" })
-  actions.floating_buffer(vim.split(results, "\n"), "k8s_pods", { title = "Top" })
+  ResourceBuilder:new("top", { "top", "pods", "-A" }):fetch():splitData():displayFloat("k8s_top", "Top", "")
 end
 
 function M.PodLogs(pod_name, namespace)
-  local results = commands.execute_shell_command("kubectl", { "logs", pod_name, "-n", namespace })
-  actions.floating_buffer(vim.split(results, "\n"), "k8s_pod_logs", { title = pod_name, syntax = "less" })
+  ResourceBuilder:new("logs", { "logs", pod_name, "-n", namespace })
+    :fetch()
+    :splitData()
+    :displayFloat("k8s_pod_logs", pod_name, "less")
 end
 
 function M.PodDesc(pod_name, namespace)
-  local desc = commands.execute_shell_command("kubectl", { "describe", "pod", pod_name, "-n", namespace })
-  actions.floating_buffer(vim.split(desc, "\n"), "k8s_pod_desc", { title = pod_name, syntax = "yaml" })
+  ResourceBuilder:new("desc", { "describe", "pod", pod_name, "-n", namespace })
+    :fetch()
+    :splitData()
+    :displayFloat("k8s_pod_desc", pod_name, "yaml")
 end
 
 function M.ExecContainer(container_name)
@@ -45,17 +49,14 @@ end
 
 function M.PodContainers(pod_name, namespace)
   selection = { pod = pod_name, ns = namespace }
-  local results = commands.execute_shell_command("kubectl", {
-    "get",
-    "pods",
-    pod_name,
-    "-n",
-    namespace,
-    "-o=json",
-  })
-
-  local data = pods.processContainerRow(vim.json.decode(results))
-  local pretty = tables.pretty_print(data, pods.getContainerHeaders())
-  actions.floating_buffer(pretty, "k8s_containers", { title = pod_name })
+  ResourceBuilder:new("containers", { "get", "pods", pod_name, "-n", namespace, "-o=json" })
+    :fetch()
+    :decodeJson()
+    :process(pods.processContainerRow)
+    :prettyPrint(pods.getContainerHeaders)
+    :addHints({
+      { key = "<enter>", desc = "exec" },
+    }, false, false)
+    :displayFloat("k8s_containers", pod_name, "")
 end
 return M
