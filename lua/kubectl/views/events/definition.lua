79749ae193e4b37a1cf81b45ddb07fb2e7e64fd5
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-06-09 22:01:11 +0200

    event lastseen sorting and correct backup source

--- lua/kubectl/views/events/definition.lua
+++ lua/kubectl/views/events/definition.lua
@@ -1,11 +1,22 @@
+local time = require("kubectl.utils.time")
 local M = {}
+
+local function getLastSeen(row)
+  if row.lastTimestamp ~= vim.NIL then
+    return time.since(row.lastTimestamp, true)
+  elseif row.eventTime ~= vim.NIL then
+    return time.since(row.eventTime, true)
+  else
+    return nil
+  end
+end
 
 function M.processRow(rows)
   local data = {}
   for _, row in pairs(rows.items) do
     local pod = {
       namespace = row.metadata.namespace,
-      lastseen = row.lastTimestamp,
+      lastseen = getLastSeen(row),
       type = row.type,
       reason = row.reason,
       object = row.involvedObject.name,
