0a969a14be116932e308976bd70712df2db43d3e
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-06-10 12:17:37 +0200

    add common event lookup, prettier event view

--- lua/kubectl/views/events/definition.lua
+++ lua/kubectl/views/events/definition.lua
@@ -1,3 +1,4 @@
+local events = require("kubectl.utils.events")
 local time = require("kubectl.utils.time")
 local M = {}
 
@@ -11,14 +12,26 @@ local function getLastSeen(row)
   end
 end
 
+local function getType(type)
+  local status = { symbol = "", value = type }
+  status.symbol = events.ColorStatus(type)
+  return status
+end
+
+local function getReason(reason)
+  local status = { symbol = "", value = reason }
+  status.symbol = events.ColorStatus(reason)
+  return status
+end
+
 function M.processRow(rows)
   local data = {}
   for _, row in pairs(rows.items) do
     local pod = {
       namespace = row.metadata.namespace,
       lastseen = getLastSeen(row),
-      type = row.type,
-      reason = row.reason,
+      type = getType(row.type),
+      reason = getReason(row.reason),
       object = row.involvedObject.name,
       count = tonumber(row.count) or 0,
       message = row.message,
