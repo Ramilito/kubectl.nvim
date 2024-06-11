8c3415970be5442b06054110af1c9b143960e0e4
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-06-11 12:18:21 +0200

    optimize a bit more, 1ms gain....

--- lua/kubectl/utils/time.lua
+++ lua/kubectl/utils/time.lua
@@ -4,11 +4,13 @@ local hl = require("kubectl.actions.highlight")
 local M = {}
 local pattern_with_frac = "(%d+)%-(%d+)%-(%d+)T(%d+):(%d+):(%d+)%.(%d+)Z"
 local pattern_without_frac = "(%d+)%-(%d+)%-(%d+)T(%d+):(%d+):(%d+)Z"
+local obj_fresh = config.options.obj_fresh
+local success_symbol = hl.symbols.success
 
 function M.parse(timestamp)
   local year, month, day, hour, min, sec, frac
   year, month, day, hour, min, sec, frac = timestamp:match(pattern_with_frac)
-  
+
   if not frac then
     year, month, day, hour, min, sec = timestamp:match(pattern_without_frac)
     frac = 0
@@ -25,27 +27,22 @@ function M.parse(timestamp)
   }) + frac / 1000000
 end
 
--- Function to get the current time in UTC
-local function getCurrentTimeUTC()
-  return os.time(os.date("!*t"))
-end
-
 -- Function to calculate the time difference and format it
 function M.since(timestamp, fresh)
-  local status = { symbol = "", value = "", timestamp = timestamp }
   if not timestamp or type(timestamp) ~= "string" then
     return nil
   end
 
   local parsedTime = M.parse(timestamp)
-  local currentTime = getCurrentTimeUTC()
+  local currentTime = os.time(os.date("!*t"))
   local diff = currentTime - parsedTime
 
-  local seconds = diff % 60
-  local minutes = math.floor(diff / 60) % 60
-  local hours = math.floor(diff / 3600) % 24
   local days = math.floor(diff / 86400)
+  local hours = math.floor((diff % 86400) / 3600)
+  local minutes = math.floor((diff % 3600) / 60)
+  local seconds = diff % 60
 
+  local status = { symbol = "", value = "", timestamp = timestamp }
   if days > 7 then
     status.value = string.format("%dd", days)
   elseif days > 0 or hours > 23 then
@@ -56,8 +53,8 @@ function M.since(timestamp, fresh)
     status.value = string.format("%dm%ds", minutes, seconds)
   end
 
-  if fresh and config.options.obj_fresh > math.floor(diff / 60) then
-    status.symbol = hl.symbols.success
+  if fresh and obj_fresh > math.floor(diff / 60) then
+    status.symbol = success_symbol
   end
 
   return status
