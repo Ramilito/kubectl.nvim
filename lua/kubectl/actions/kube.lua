d11741e26b8722263a932c00b905a2119d552d9b
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-06-17 10:28:36 +0200

    prettier kube.lua code

--- lua/kubectl/actions/kube.lua
+++ lua/kubectl/actions/kube.lua
@@ -4,28 +4,7 @@ local timeme = require("kubectl.utils.timeme")
 local M = {}
 M.handle = {}
 M.pid = -1
-M.stdout = {}
 
--- Function to handle stdout data
-local function handle_stdout(err, data, callback)
-  timeme.stop()
-  if err then
-    print("Error reading stdout:", err)
-    return
-  end
-  if data then
-    callback(data)
-  end
-end
-
--- Function to handle process exit
-local function handle_exit(code, signal, stdout, stderr)
-  print("kubectl proxy exited with code", code, "and signal", signal)
-  stdout:close()
-  stderr:close()
-end
-
--- Function to stop the kubectl proxy process
 function M.stop_kubectl_proxy()
   return function()
     if M.handle and not M.handle:is_closing() then
@@ -36,13 +15,11 @@ function M.stop_kubectl_proxy()
   end
 end
 
--- Function to start the kubectl proxy process
 function M.startProxy(callback)
   timeme.start()
   local stdout = uv.new_pipe(false)
   local stderr = uv.new_pipe(false)
 
-  -- Spawn the kubectl proxy process
   M.handle, M.pid = uv.spawn(
     "kubectl",
     {
@@ -50,30 +27,35 @@ function M.startProxy(callback)
       stdio = { nil, stdout, stderr },
     },
     vim.schedule_wrap(function(code, signal)
-      handle_exit(code, signal, stdout, stderr)
+      print("kubectl proxy exited with code", code, "and signal", signal)
+      stdout:close()
+      stderr:close()
     end)
   )
 
-  -- Check if the process failed to start
   if not M.handle then
     print("Failed to start kubectl proxy")
     return nil
   end
 
-  -- Start reading stdout
   uv.read_start(
     stdout,
     vim.schedule_wrap(function(err, data)
-      handle_stdout(err, data, callback)
+      timeme.stop()
+      if err then
+        print("Error reading stdout:", err)
+        return
+      end
+      if data then
+        callback(data)
+      end
     end)
   )
 
-  -- Set up an autocommand to stop the kubectl proxy when Neovim exits
   vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = M.stop_kubectl_proxy(),
   })
 
-  -- Return the process handle for reference
   return M.handle
 end
 
