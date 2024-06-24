cb8d092307788b0cc12c17c8e16bbe2d231f052b
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-06-24 22:30:37 +0200

    simplify kube proxy code

--- lua/kubectl/actions/kube.lua
+++ lua/kubectl/actions/kube.lua
@@ -1,4 +1,3 @@
-local uv = vim.loop
 local state = require("kubectl.state")
 
 local M = {}
@@ -8,64 +7,43 @@ M.pid = -1
 function M.stop_kubectl_proxy()
   return function()
     if M.handle and not M.handle:is_closing() then
-      uv.kill(M.pid, function()
-        print("process closed", M.handle, M.pid)
-      end)
+      M.handle:kill(2)
     end
   end
 end
 
 function M.startProxy(callback)
-  local stdout = uv.new_pipe(false)
-  local stderr = uv.new_pipe(false)
-
-  M.handle, M.pid = uv.spawn(
-    "kubectl",
-    {
-      args = { "proxy", "--port=0" },
-      stdio = { nil, stdout, stderr },
-    },
-    vim.schedule_wrap(function(code, signal)
-      print("kubectl proxy exited with code", code, "and signal", signal)
-      stdout:close()
-      stderr:close()
-    end)
-  )
-
-  if not M.handle then
-    print("Failed to start kubectl proxy")
-    return nil
+  local function on_stdout(err, data)
+    if err then
+      print("Error reading stdout:", err)
+      return
+    end
+    if data then
+      local port = data:match(":(%d+)")
+      if port then
+        port = tonumber(port)
+        if port and port > 0 and port <= 65535 then
+          state.setProxyUrl(port)
+          callback()
+        else
+          print("Invalid port number:", port)
+        end
+      else
+        print("No port number found in the output.")
+      end
+    end
   end
 
-  uv.read_start(
-    stdout,
-    vim.schedule_wrap(function(err, data)
-      if err then
-        print("Error reading stdout:", err)
-        return
-      end
-      if data then
-        local port = data:match(":(%d+)")
-        if port then
-          port = tonumber(port)
-          if port and port > 0 and port <= 65535 then
-            state.setProxyUrl(port)
-            callback()
-          else
-            print("Invalid port number: " .. tostring(port))
-          end
-        else
-          print("No port number found in the text.")
-        end
-      end
-    end)
-  )
+  M.handle = vim.system({ "kubectl", "proxy", "--port=0" }, {
+    stdin = false,
+    stderr = false,
+    stdout = on_stdout,
+    detach = false,
+  }, M.stop_kubectl_proxy())
 
   vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = M.stop_kubectl_proxy(),
   })
-
-  return M.handle
 end
 
 return M
