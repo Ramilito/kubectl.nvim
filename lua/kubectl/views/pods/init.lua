f76d207b151c49705ad653e09b5ba47a30210fd8
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-07-06 21:13:15 +0200

    apply to all terminal instances

--- lua/kubectl/views/pods/init.lua
+++ lua/kubectl/views/pods/init.lua
@@ -82,7 +82,6 @@ end
 function M.Edit(name, namespace)
   buffers.floating_buffer({}, {}, "yaml", { title = "Edit pod", syntax = "yaml" })
   commands.execute_terminal("kubectl", { "edit", "pod/" .. name, "-n", namespace })
-  vim.cmd("startinsert")
 end
 
 function M.PodDesc(pod_name, namespace)
