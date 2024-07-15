12fa95de4e64081276f32832920416c150cf6753
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-07-15 09:14:40 +0200

    add space

--- lua/kubectl/resourcebuilder.lua
+++ lua/kubectl/resourcebuilder.lua
@@ -178,7 +178,7 @@ function ResourceBuilder:addHints(hints, include_defaults, include_context)
     divider_text = divider_text .. "[" .. #self.data - 1 .. "]"
   end
   if state.filter ~= "" then
-    divider_text = divider_text .. "</" .. state.filter .. ">"
+    divider_text = divider_text .. " " .. "</" .. state.filter .. ">"
   end
   self.header.data, self.header.marks = tables.generateHeader(hints, include_defaults, include_context, divider_text)
   return self
