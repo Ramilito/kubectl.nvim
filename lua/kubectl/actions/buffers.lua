2576f8d9d6896bcd759eda103d664189dd5a7f85
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-07-16 23:20:57 +0200

    use bdelete instead of close to get rid of window

--- lua/kubectl/actions/buffers.lua
+++ lua/kubectl/actions/buffers.lua
@@ -181,7 +181,9 @@ function M.floating_buffer(content, marks, filetype, opts)
   set_buffer_lines(buf, opts.header.data, content)
 
   local win = layout.float_layout(buf, filetype, opts.title or "")
-  vim.keymap.set("n", "q", vim.cmd.close, { buffer = buf, silent = true })
+  vim.keymap.set("n", "q", function()
+    vim.cmd("bdelete")
+  end, { buffer = buf, silent = true })
 
   layout.set_buf_options(buf, win, filetype, opts.syntax or filetype, bufname)
   apply_marks(buf, marks, opts.header)
