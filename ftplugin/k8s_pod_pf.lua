2ee79ecca035c60e30dfe425d609aa33add7b383
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-07-23 23:07:04 +0200

    kill pf functionality

--- ftplugin/k8s_pod_pf.lua
+++ ftplugin/k8s_pod_pf.lua
@@ -1,19 +1,21 @@
+local commands = require("kubectl.actions.commands")
+local pod_view = require("kubectl.views.pods")
 local tables = require("kubectl.utils.tables")
 
---- Set key mappings for the buffer
 local function set_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gk", "", {
     noremap = true,
     silent = true,
     desc = "Kill port forward",
     callback = function()
-      local pid = tables.getCurrentSelection(1)
-      print(pid)
+      local pid, resource = tables.getCurrentSelection(1, 2)
+      vim.notify("Killing port forward for resource " .. resource .. " with pid: " .. pid)
+      commands.shell_command("kill", { pid })
+      pod_view.PodPF()
     end,
   })
 end
 
---- Initialize the module
 local function init()
   set_keymaps(0)
 end
