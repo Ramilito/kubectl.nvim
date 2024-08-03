a2fca49e8bf5638ccbd219f3bdb1cb0d0fdaaf28
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-03 23:40:31 +0200

    add hints to desc event

--- lua/kubectl/views/events/init.lua
+++ lua/kubectl/views/events/init.lua
@@ -14,6 +14,7 @@ function M.View(cancellationToken)
       vim.schedule(function()
         self
           :addHints({
+            { key = "<gd>", desc = "describe" },
             { key = "<enter>", desc = "message" },
           }, true, true, true)
           :display("k8s_events", "Events", cancellationToken)
