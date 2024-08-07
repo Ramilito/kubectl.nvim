25d68a4a92fe9410302d2963e3decb051570e3a7
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-07 08:19:04 +0200

    display and allow choosing history data

--- lua/kubectl/actions/layout.lua
+++ lua/kubectl/actions/layout.lua
@@ -41,7 +41,7 @@ end
 --- @return integer: The window number.
 function M.filter_layout(buf, filetype, title)
   local width = 0.8 * vim.o.columns
-  local height = 5
+  local height = 13
   local row = 10
   local col = 10
 
--- lua/kubectl/views/filter/init.lua
+++ lua/kubectl/views/filter/init.lua
@@ -4,6 +4,7 @@ local tables = require("kubectl.utils.tables")
 
 local M = {}
 
+M.old_filter = { "test", "pest", "foo", "bar" }
 function M.filter()
   local buf = buffers.filter_buffer("k8s_filter", { title = "Filter", header = { data = {} } })
   local header, marks = tables.generateHeader({
@@ -11,10 +12,36 @@ function M.filter()
     { key = "<q>", desc = "close" },
   }, false, false)
 
+  table.insert(header, "History:")
+  for _, value in ipairs(M.old_filter) do
+    table.insert(header, value)
+  end
+
+  table.insert(header, "")
+
   vim.api.nvim_buf_set_lines(buf, 0, #header, false, header)
   vim.api.nvim_buf_set_lines(buf, #header, -1, false, { "Filter: " .. state.getFilter(), "" })
 
   buffers.apply_marks(buf, marks, header)
+
+  vim.api.nvim_buf_set_keymap(buf, "n", "<cr>", "", {
+    noremap = true,
+    callback = function()
+      local line = vim.api.nvim_get_current_line()
+
+      -- Don't act on prompt line
+      local current_line = vim.api.nvim_win_get_cursor(0)[1]
+      if current_line >= #header then
+        return
+      end
+
+      local prompt = "% "
+
+      vim.api.nvim_buf_set_lines(buf, #header + 1, -1, false, { prompt .. line })
+      vim.api.nvim_win_set_cursor(0, { #header + 2, #prompt })
+      vim.cmd("startinsert")
+    end,
+  })
 end
 
 return M
