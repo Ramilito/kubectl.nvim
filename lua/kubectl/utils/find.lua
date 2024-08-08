b6b6adc399259d904538c2a568d164365552ea56
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-08 08:44:24 +0200

    removed redundant loop

--- lua/kubectl/utils/find.lua
+++ lua/kubectl/utils/find.lua
@@ -40,7 +40,7 @@ local function is_in_table(tbl, str)
       if is_in_table(value, str) then
         return true
       end
-    elseif value:lower():match(lowered_str) then
+    elseif tostring(value):lower():match(lowered_str) then
       return true
     end
   end
@@ -53,21 +53,17 @@ end
 ---@param startAt number
 ---@return table[]
 function M.filter_line(array, pattern, startAt)
-  local filtered_array = {}
   if not pattern then
     return array
   end
   startAt = startAt or 1
+  local filtered_array = {}
 
-  if array then
-    for index = 1, startAt - 1 do
-      table.insert(filtered_array, array[index])
-    end
-    for index = startAt, #array do
-      local line = array[index]
-      if is_in_table(line, pattern) then
-        table.insert(filtered_array, line)
-      end
+  -- Filter the array starting from startAt
+  for index = startAt, #array do
+    local line = array[index]
+    if is_in_table(line, pattern) then
+      table.insert(filtered_array, line)
     end
   end
 
