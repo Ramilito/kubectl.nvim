38c77ed202b9a8e84f74fa0ccdeeb08a576044fe
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-11 18:49:59 +0200

    add nodeselector column

--- lua/kubectl/views/daemonsets/definition.lua
+++ lua/kubectl/views/daemonsets/definition.lua
@@ -2,19 +2,36 @@ local M = {}
 local hl = require("kubectl.actions.highlight")
 local time = require("kubectl.utils.time")
 
+local function getNodeSelector(row)
+  local ns = row.spec.template.spec.nodeSelector
+  if ns then
+    local result = ""
+    for key, value in pairs(ns) do
+      if result ~= "" then
+        result = result .. ","
+      end
+      result = result .. key .. "=" .. value
+    end
+    return result
+  end
+  return "<none>"
+end
+
 function M.processRow(rows)
   local data = {}
-  print(vim.inspect(rows))
   if rows and rows.items then
     for _, row in pairs(rows.items) do
+      local status = row.status
+
       local pod = {
         namespace = row.metadata.namespace,
         name = row.metadata.name,
-        desired = row.status.desiredNumberScheduled,
-        current = row.status.currentNumberScheduled,
+        desired = status.desiredNumberScheduled or 0,
+        current = status.currentNumberScheduled or 0,
         ready = M.getReady(row),
-        uptodate = row.status.updatedNumberScheduled,
-        available = row.status.numberAvailable,
+        uptodate = status.updatedNumberScheduled or 0,
+        available = status.numberAvailable or 0,
+        nodeselector = getNodeSelector(row),
         age = time.since(row.metadata.creationTimestamp, true),
       }
 
@@ -44,7 +61,7 @@ function M.getReady(row)
   local status = { symbol = "", value = "" }
   local numberReady = row.status.numberReady
   if numberReady then
-    status.value = numberReady .. "/" .. row.status.numberAvailable
+    status.value = numberReady .. "/" .. (row.status.numberAvailable or 0)
     if numberReady == row.status.numberAvailable then
       status.symbol = hl.symbols.note
     else
