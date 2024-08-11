51a66546a04f7b4642f2488ef9502543ace35d5a
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-12 00:40:38 +0200

    add crds view

---
+++ ftplugin/k8s_crds.lua
@@ -0,0 +1,37 @@
+local loop = require("kubectl.utils.loop")
+local root_view = require("kubectl.views.root")
+local api = vim.api
+local crds_view = require("kubectl.views.crds")
+local view = require("kubectl.views")
+
+--- Set key mappings for the buffer
+local function set_keymaps(bufnr)
+  api.nvim_buf_set_keymap(bufnr, "n", "g?", "", {
+    noremap = true,
+    silent = true,
+    desc = "Help",
+    callback = function()
+      view.Hints({ { key = "<gd>", desc = "Describe selected pod" } })
+    end,
+  })
+
+  api.nvim_buf_set_keymap(bufnr, "n", "<bs>", "", {
+    noremap = true,
+    silent = true,
+    desc = "Go up",
+    callback = function()
+      root_view.View()
+    end,
+  })
+end
+
+--- Initialize the module
+local function init()
+  set_keymaps(0)
+
+  if not loop.is_running() then
+    loop.start_loop(crds_view.View)
+  end
+end
+
+init()
---
+++ lua/kubectl/views/crds/init.lua
@@ -0,0 +1,51 @@
+local ResourceBuilder = require("kubectl.resourcebuilder")
+local buffers = require("kubectl.actions.buffers")
+local commands = require("kubectl.actions.commands")
+local definition = require("kubectl.views.crds.definition")
+local tables = require("kubectl.utils.tables")
+
+local M = {}
+
+function M.View(cancellationToken)
+  ResourceBuilder:new("crds")
+    :display("k8s_crds", "CRDS", cancellationToken)
+    :setCmd({ "{{BASE}}/apis/apiextensions.k8s.io/v1/customresourcedefinitions?pretty=false" }, "curl")
+    :fetchAsync(function(self)
+      self:decodeJson():process(definition.processRow):sort():prettyPrint(definition.getHeaders)
+
+      vim.schedule(function()
+        self
+          :addHints({
+            { key = "<gd>", desc = "describe" },
+          }, true, true, true)
+          :setContent(cancellationToken)
+      end)
+    end)
+end
+
+--- Edit a configmap
+---@param name string
+function M.Edit(name)
+  buffers.floating_buffer("k8s_crds_edit", name, "yaml")
+  commands.execute_terminal("kubectl", { "edit", "crds/" .. name })
+end
+
+--- Describe a configmap
+---@param name string
+---@param ns string
+function M.Desc(name)
+  ResourceBuilder:new("desc")
+    :displayFloat("k8s_crds_desc", name, "yaml")
+    :setCmd({ "describe", "crds", name })
+    :fetch()
+    :splitData()
+    :setContentRaw()
+end
+
+--- Get current seletion for view
+---@return string|nil
+function M.getCurrentSelection()
+  return tables.getCurrentSelection(1)
+end
+
+return M
