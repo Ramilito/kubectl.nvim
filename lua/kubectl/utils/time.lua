df7442a6d0f3667cc8a9f320ffb57985231bf7c8
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-14 20:54:18 +0200

    removed parse function

--- lua/kubectl/utils/time.lua
+++ lua/kubectl/utils/time.lua
@@ -2,32 +2,8 @@ local config = require("kubectl.config")
 local hl = require("kubectl.actions.highlight")
 
 local M = {}
-local pattern = "(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)%.?(%d*)"
 local obj_fresh = config.options.obj_fresh
 local success_symbol = hl.symbols.success
-
---- Parse a timestamp into an os.time value
----@param timestamp string
----@return number
-function M.parse(timestamp)
-  local year, month, day, hour, min, sec, frac
-  year, month, day, hour, min, sec, frac = timestamp:match(pattern)
-
-  frac = frac ~= "" and frac or 0
-
-  local localTime = os.time({
-    year = year,
-    month = month,
-    day = day,
-    hour = hour,
-    min = min,
-    sec = sec,
-    isdst = false, -- Explicitly setting isdst to false
-  }) + frac / 1000000
-
-  ---@diagnostic disable-next-line: param-type-mismatch
-  return os.time(os.date("!*t", localTime))
-end
 
 --- Calculate the time difference since the given timestamp and format it
 ---@param timestamp string
