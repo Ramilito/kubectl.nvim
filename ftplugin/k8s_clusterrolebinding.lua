942c0ceebe9fde661460c9bea4390caa09382b28
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-08-16 12:15:51 +0200

    add crb view

---
+++ ftplugin/k8s_clusterrolebinding.lua
@@ -0,0 +1,26 @@
+local clusterrolebinding_view = require("kubectl.views.clusterrolebinding")
+local definition = require("kubectl.views.clusterrolebinding.definition")
+local loop = require("kubectl.utils.loop")
+local view = require("kubectl.views")
+
+--- Set key mappings for the buffer
+local function set_keymaps(bufnr)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "g?", "", {
+    noremap = true,
+    silent = true,
+    desc = "Help",
+    callback = function()
+      view.Hints(definition.hints)
+    end,
+  })
+end
+
+--- Initialize the module
+local function init()
+  set_keymaps(0)
+  if not loop.is_running() then
+    loop.start_loop(clusterrolebinding_view.View)
+  end
+end
+
+init()
