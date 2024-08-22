1b6d71c38a86208778000af5f1950f55b4c4236a
Moshe Avni <mavni@netapp.com>
2024-08-22 23:19:19 +0300

    fix test

--- lua/kubectl/views/definition.lua
+++ lua/kubectl/views/definition.lua
@@ -98,7 +98,7 @@ function M.on_prompt_input(input)
   local parsed_input = string.lower(string_utils.trim(input))
   local supported_view = nil
   for k, v in pairs(viewsTable) do
-    if find.is_in_table(v, parsed_input, true) then
+    if find.is_in_table(v, parsed_input) then
       supported_view = k
     end
   end
