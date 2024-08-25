e8a63f07b7b2d32d63faf2853dd1d0720a26b03b
Moshe Avni <mavni@netapp.com>
2024-08-25 17:08:40 +0300

    don't export internal functions

--- lua/kubectl/views/cronjobs/definition.lua
+++ lua/kubectl/views/cronjobs/definition.lua
@@ -11,6 +11,32 @@ local M = {
 }
 local hl = require("kubectl.actions.highlight")
 local time = require("kubectl.utils.time")
+local function getActive(row)
+  local active = row.status.active
+  if active == nil then
+    return 0
+  end
+  return #active
+end
+
+local function getSelector(row)
+  local selector = {}
+  if row.spec.selector == nil then
+    return "<none>"
+  end
+  for key, value in pairs(row.spec.selector.matchLabels) do
+    table.insert(selector, key .. "=" .. value)
+  end
+  return table.concat(selector, ",")
+end
+
+local function getContainerData(row, key)
+  local containers = {}
+  for _, container in ipairs(row.spec.jobTemplate.spec.template.spec.containers) do
+    table.insert(containers, container[key])
+  end
+  return table.concat(containers, ",")
+end
 
 function M.processRow(rows)
   local data = {}
@@ -26,11 +52,11 @@ function M.processRow(rows)
         name = row.metadata.name,
         schedule = row.spec.schedule,
         suspend = { symbol = row.spec.suspend and hl.symbols.error or hl.symbols.success, value = row.spec.suspend },
-        active = M.getActive(row),
+        active = getActive(row),
         ["last schedule"] = time.since(row.status.lastScheduleTime, true) or "",
-        containers = M.getContainerData(row, "name"),
-        images = M.getContainerData(row, "image"),
-        selector = M.getSelector(row),
+        containers = getContainerData(row, "name"),
+        images = getContainerData(row, "image"),
+        selector = getSelector(row),
         age = time.since(row.metadata.creationTimestamp, true),
       }
 
@@ -55,33 +81,6 @@ function M.getHeaders()
   }
 
   return headers
-end
-
-function M.getActive(row)
-  local active = row.status.active
-  if active == nil then
-    return 0
-  end
-  return #active
-end
-
-function M.getSelector(row)
-  local selector = {}
-  if row.spec.selector == nil then
-    return "<none>"
-  end
-  for key, value in pairs(row.spec.selector.matchLabels) do
-    table.insert(selector, key .. "=" .. value)
-  end
-  return table.concat(selector, ",")
-end
-
-function M.getContainerData(row, key)
-  local containers = {}
-  for _, container in ipairs(row.spec.jobTemplate.spec.template.spec.containers) do
-    table.insert(containers, container[key])
-  end
-  return table.concat(containers, ",")
 end
 
 return M
--- lua/kubectl/views/jobs/definition.lua
+++ lua/kubectl/views/jobs/definition.lua
@@ -12,6 +12,49 @@ local M = {
 local hl = require("kubectl.actions.highlight")
 local time = require("kubectl.utils.time")
 
+local function getDuration(row)
+  local is_complete = row.status.completionTime
+  local duration
+  if is_complete then
+    duration = time.since(row.metadata.creationTimestamp, false, vim.fn.strptime("%Y-%m-%dT%H:%M:%SZ", is_complete))
+  else
+    duration = time.since(row.metadata.creationTimestamp, false)
+  end
+  return duration
+end
+
+local function getSelector(row)
+  local selector = {}
+  if row.spec.selector == nil then
+    return "<none>"
+  end
+  for key, value in pairs(row.spec.selector.matchLabels) do
+    table.insert(selector, key .. "=" .. value)
+  end
+  return table.concat(selector, ",")
+end
+
+local function getCompletions(row)
+  local completions = { symbol = "", value = "" }
+  local desired = row.spec.completions
+  local actual = row.status.succeeded or 0
+  if desired == actual then
+    completions.symbol = hl.symbols.note
+  else
+    completions.symbol = hl.symbols.deprecated
+  end
+  completions.value = actual .. "/" .. desired
+  return completions
+end
+
+local function getContainerData(row, key)
+  local containers = {}
+  for _, container in ipairs(row.spec.template.spec.containers) do
+    table.insert(containers, container[key])
+  end
+  return table.concat(containers, ",")
+end
+
 function M.processRow(rows)
   local data = {}
 
@@ -24,11 +67,11 @@ function M.processRow(rows)
       local job = {
         namespace = row.metadata.namespace,
         name = row.metadata.name,
-        completions = M.getCompletions(row),
-        duration = M.getDuration(row),
-        containers = M.getContainerData(row, "name"),
-        images = M.getContainerData(row, "image"),
-        selector = M.getSelector(row),
+        completions = getCompletions(row),
+        duration = getDuration(row),
+        containers = getContainerData(row, "name"),
+        images = getContainerData(row, "image"),
+        selector = getSelector(row),
         age = time.since(row.metadata.creationTimestamp, true),
       }
 
@@ -51,49 +94,6 @@ function M.getHeaders()
   }
 
   return headers
-end
-
-function M.getDuration(row)
-  local is_complete = row.status.completionTime
-  local duration
-  if is_complete then
-    duration = time.since(row.metadata.creationTimestamp, false, vim.fn.strptime("%Y-%m-%dT%H:%M:%SZ", is_complete))
-  else
-    duration = time.since(row.metadata.creationTimestamp, false)
-  end
-  return duration
-end
-
-function M.getSelector(row)
-  local selector = {}
-  if row.spec.selector == nil then
-    return "<none>"
-  end
-  for key, value in pairs(row.spec.selector.matchLabels) do
-    table.insert(selector, key .. "=" .. value)
-  end
-  return table.concat(selector, ",")
-end
-
-function M.getCompletions(row)
-  local completions = { symbol = "", value = "" }
-  local desired = row.spec.completions
-  local actual = row.status.succeeded or 0
-  if desired == actual then
-    completions.symbol = hl.symbols.note
-  else
-    completions.symbol = hl.symbols.deprecated
-  end
-  completions.value = actual .. "/" .. desired
-  return completions
-end
-
-function M.getContainerData(row, key)
-  local containers = {}
-  for _, container in ipairs(row.spec.template.spec.containers) do
-    table.insert(containers, container[key])
-  end
-  return table.concat(containers, ",")
 end
 
 return M
