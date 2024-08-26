e8a4af37cdb10810aebc2d71647e90af998db6b5
Moshe Avni <mavni@netapp.com>
2024-08-26 16:43:43 +0300

    add hint

--- lua/kubectl/views/pvc/definition.lua
+++ lua/kubectl/views/pvc/definition.lua
@@ -7,6 +7,7 @@ local M = {
   url = { "{{BASE}}/api/v1/{{NAMESPACE}}persistentvolumeclaims?pretty=false" },
   hints = {
     { key = "<gd>", desc = "describe", long_desc = "Describe selected pvc" },
+    { key = "<enter>", desc = "Go to PVs", long_desc = "Go to the PV of the selected PVC" },
   },
 }
 
