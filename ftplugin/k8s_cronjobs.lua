4c7b001e1c1eb4d46fdb37dcde81828f337bf363
Moshe Avni <mavni@netapp.com>
2024-08-26 10:45:08 +0300

    Feat/add jobs and cronjobs (#226)

---
+++ ftplugin/k8s_cronjobs.lua
@@ -0,0 +1,72 @@
+local api = vim.api
+local commands = require("kubectl.actions.commands")
+local cronjob_view = require("kubectl.views.cronjobs")
+local definition = require("kubectl.views.cronjobs.definition")
+local loop = require("kubectl.utils.loop")
+local root_view = require("kubectl.views.root")
+local view = require("kubectl.views")
+
+--- Set key mappings for the buffer
+local function set_keymaps(bufnr)
+  api.nvim_buf_set_keymap(bufnr, "n", "g?", "", {
+    noremap = true,
+    silent = true,
+    desc = "Help",
+    callback = function()
+      view.Hints(definition.hints)
+    end,
+  })
+
+  api.nvim_buf_set_keymap(bufnr, "n", "<CR>", "", {
+    noremap = true,
+    silent = true,
+    desc = "Go to jobs",
+    callback = function()
+      local name, ns = cronjob_view.getCurrentSelection()
+      local job_view = require("kubectl.views.jobs")
+      job_view.View(nil, { name = name, ns = ns })
+    end,
+  })
+
+  api.nvim_buf_set_keymap(bufnr, "n", "<bs>", "", {
+    noremap = true,
+    silent = true,
+    desc = "Go up",
+    callback = function()
+      root_view.View()
+    end,
+  })
+
+  api.nvim_buf_set_keymap(bufnr, "n", "grr", "", {
+    noremap = true,
+    silent = true,
+    desc = "Create job from cronjob",
+    callback = function()
+      local name, ns = cronjob_view.getCurrentSelection()
+      vim.ui.input({ prompt = "New job name " }, function(input)
+        if not input or input == "" then
+          return
+        end
+        commands.shell_command_async(
+          "kubectl",
+          { "create", "job", input, "--from", "cronjobs/" .. name, "-n", ns },
+          function(response)
+            vim.schedule(function()
+              vim.notify(response)
+            end)
+          end
+        )
+      end)
+    end,
+  })
+end
+
+--- Initialize the module
+local function init()
+  set_keymaps(0)
+  if not loop.is_running() then
+    loop.start_loop(cronjob_view.Draw)
+  end
+end
+
+init()
