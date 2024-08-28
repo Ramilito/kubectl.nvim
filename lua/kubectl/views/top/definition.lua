b4bd8543cde423216d077243afc4b7a90d3e3d73
Moshe Avni <mavni@netapp.com>
2024-08-28 18:09:57 +0300

    done

--- lua/kubectl/views/top/definition.lua
+++ lua/kubectl/views/top/definition.lua
@@ -45,41 +45,50 @@ local function getCpuUsage(row, node)
 end
 
 local function getMemUsage(row, node)
+  local function split_num_unit(mem)
+    local num = tonumber(string.sub(mem, 1, -3)) or 0
+    local unit = string.sub(mem, -2) or "Ki"
+    return num, unit
+  end
+  local function get_ki_val(mem)
+    local mem_val, unit = split_num_unit(mem)
+    if unit == "Mi" then
+      mem_val = math.floor(mem_val * 1024)
+    end
+    return mem_val
+  end
+  local function kib_to_mib_or_gib(mem)
+    local final_val = math.floor(mem / 1024)
+    local unit = "Mi"
+    if  final_val > 10240 then
+      final_val = math.floor(final_val / 1024)
+      unit = "Gi"
+    end
+    return final_val, unit
+  end
   local status = { symbol = "", value = "", sort_by = "" }
   local temp_val = 0
   local out_of = ""
   if row.containers then
     for _, container in pairs(row.containers) do
       local mem = container.usage.memory
-      local unit = string.sub(mem, -2) or "Ki"
-      local mem_val = tonumber(string.sub(mem, 1, -3)) or 0
-      if unit == "Mi" then
-        mem_val = math.floor(mem_val * 1024)
-      end
-      temp_val = temp_val + mem_val
+      temp_val = temp_val + get_ki_val(mem)
     end
   elseif row.usage.memory then
     local mem = row.usage.memory
     temp_val = tonumber(string.sub(mem, 1, -3)) or 0
     out_of = node and node.status and node.status.capacity.memory or ""
     if out_of ~= "" then
-      local v = math.floor(tonumber(string.sub(out_of, 1, -3)) / 1024) or 0
-      local percent = math.ceil((math.floor(temp_val / 1024) / v) * 100)
-      if v > 10240 then
-        out_of = "/" .. math.floor(v / 1024) .. "Gi (" .. percent .. "%)"
-      else
-        out_of = "/" .. v .. "Mi (" .. percent .. "%)"
-      end
+      local kib = get_ki_val(out_of)
+      local new_mem_val, new_unit = kib_to_mib_or_gib(kib)
+      local percent = math.ceil((temp_val / kib) * 100)
+      out_of = "/" .. new_mem_val .. new_unit .. " (" .. percent .. "%)"
     end
   end
 
   status.sort_by = temp_val
-  local final_val = math.floor(temp_val / 1024)
-  if final_val > 10240 then
-    status.value = math.floor(final_val / 1024) .. "Gi" .. out_of
-  else
-    status.value = final_val .. "Mi" .. out_of
-  end
+  local final_val, unit = kib_to_mib_or_gib(temp_val)
+  status.value = final_val .. unit .. out_of
   return status
 end
 
