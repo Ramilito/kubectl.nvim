c4b3ceafc7e5b27bdfd158639c36d6fce17a0d71
Moshe Avni <mavni@netapp.com>
2024-08-28 12:42:47 +0300

    accumulate all containers cpu and mem

--- lua/kubectl/views/top/definition.lua
+++ lua/kubectl/views/top/definition.lua
@@ -15,33 +15,49 @@ local hl = require("kubectl.actions.highlight")
 local time = require("kubectl.utils.time")
 
 local function getCpuUsage(row)
-  local status = { symbol = "", value = "", sort_by = "" }
-  local cpu = row.containers and row.containers[1].usage.cpu or row.usage.cpu
-  status.value = cpu
-  cpu = string.sub(cpu, 1, -2)
-  status.sort_by = tonumber(cpu)
-  if not cpu or cpu == nil or tonumber(cpu) == nil then
-    return status
+  local status = { symbol = "", value = "", sort_by = 0 }
+  local temp_val = 0
+  if row.containers then
+    for _, container in pairs(row.containers) do
+      local cpu = container.usage.cpu
+      local cpu_val = tonumber(string.sub(cpu, 1, -2)) or 0
+      temp_val = temp_val + cpu_val
+    end
+  elseif row.usage.cpu then
+    local cpu = row.usage.cpu
+    temp_val = tonumber(string.sub(cpu, 1, -2)) or 0
   end
-  cpu = tonumber(cpu) / 1000000
-  cpu = math.ceil(cpu)
-  status.value = cpu .. "m"
+
+  status.sort_by = temp_val
+  status.value = math.ceil(temp_val / 1000000) .. "m"
   return status
 end
 
 local function getMemUsage(row)
   local status = { symbol = "", value = "", sort_by = "" }
-  -- last 2 characters are "Ki"
-  local size_str = row.containers and row.containers[1].usage.memory or row.usage.memory
-  local unit = string.sub(size_str, -2) or "Ki"
-  status.sort_by = tonumber(string.sub(size_str, 1, -3) or 0)
-  if unit == "Ki" then
-    unit = "Mi"
-    size_str = string.sub(size_str, 1, -3)
-    size_str = math.floor(tonumber(size_str) / 1024)
+  local temp_val = 0
+  if row.containers then
+    for _, container in pairs(row.containers) do
+      local mem = container.usage.memory
+      local unit = string.sub(mem, -2) or "Ki"
+      local mem_val = tonumber(string.sub(mem, 1, -3)) or 0
+      if unit == "Mi" then
+        mem_val = math.floor(mem_val * 1024)
+      end
+      temp_val = temp_val + mem_val
+    end
+  elseif row.usage.memory then
+    local mem = row.usage.memory
+    temp_val = tonumber(string.sub(mem, 1, -3)) or 0
   end
-  status.value = size_str .. unit
-  vim.print("value: " .. status.value .. " sort_by: " .. status.sort_by)
+
+  status.sort_by = temp_val
+  local final_val = math.floor(temp_val / 1024)
+  if final_val > 10240 then
+    status.value = math.floor(final_val / 1024) .. "Gi"
+  else
+    status.value = final_val .. "Mi"
+  end
   return status
 end
 
