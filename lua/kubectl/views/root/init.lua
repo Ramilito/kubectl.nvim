1fc3871f9d435e6932bbab893a268c978195c014
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-09-10 01:14:13 +0200

    working multi await data populate

--- lua/kubectl/views/root/init.lua
+++ lua/kubectl/views/root/init.lua
@@ -1,25 +1,45 @@
 local ResourceBuilder = require("kubectl.resourcebuilder")
+local commands = require("kubectl.actions.commands")
 local definition = require("kubectl.views.root.definition")
 local grid = require("kubectl.utils.grid")
 local timeme = require("kubectl.utils.timeme")
+local url = require("kubectl.utils.url")
 
 local M = {}
 
 function M.View()
   timeme.start()
-  ResourceBuilder:new(definition.resource)
-    :display(definition.ft, definition.resource)
-    :setCmd(definition.url, definition.cmd)
-    :fetchAsync(function(builder)
-      builder:decodeJson():process(definition.processRow):sort()
-      vim.schedule(function()
-        builder.prettyData, builder.extmarks =
-          grid.pretty_print(definition.processRow(builder.data), definition.getSections())
-        builder:addHints(definition.hints, true, true, true):setContent(nil)
-      end)
 
-      timeme.stop()
-    end)
+  local builder = ResourceBuilder:new(definition.resource)
+  local cmds = {
+    {
+      cmd = "curl",
+      args = { "{{{BASE}}/apis/metrics.k8s.io/v1beta1/nodes?pretty=false}" },
+      contentType = "json",
+    },
+    {
+      cmd = "curl",
+      args = { "{{{BASE}}/apis/metrics.k8s.io/v1beta1/pods?pretty=false}" },
+      contentType = "json",
+    },
+  }
+
+  for _, cmd in ipairs(cmds) do
+    if cmd.cmd == "curl" then
+      cmd.args = url.build(cmd.args)
+      cmd.args = url.addHeaders(cmd.args, cmd.contentType)
+    else
+    end
+  end
+
+  builder.data = commands.await_shell_command_async(cmds)
+  builder:display(definition.ft, definition.resource)
+  builder:decodeJson():process(definition.processRow, true)
+
+  builder.prettyData, builder.extmarks = grid.pretty_print(builder.processedData, definition.getSections())
+  builder:addHints(definition.hints, true, true, true):setContent(nil)
+
+  timeme.stop()
 end
 
 return M
