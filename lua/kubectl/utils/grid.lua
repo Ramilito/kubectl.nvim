dc35eddc35fd2fec818d8dec14bcd6e9862e2258
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-09-09 23:34:41 +0200

    add extra padding to fill width

--- lua/kubectl/utils/grid.lua
+++ lua/kubectl/utils/grid.lua
@@ -1,6 +1,20 @@
 local hl = require("kubectl.actions.highlight")
 local M = {}
 
+local function calculate_extra_padding(widths, headers)
+  local win = vim.api.nvim_get_current_win()
+  local win_width = vim.api.nvim_win_get_width(win)
+  local text_width = win_width - vim.fn.getwininfo(win)[1].textoff
+  local total_width = 0
+
+  for key, value in pairs(widths) do
+    local max_width = math.max(#key, value)
+    total_width = total_width + max_width
+    widths[key] = max_width
+  end
+  return math.floor(math.max((text_width - total_width) / #headers - 1, 0))
+end
+
 local function section_widths(rows, sections)
   local widths = {}
 
@@ -28,13 +42,14 @@ function M.pretty_print(data, sections)
   local dash = "—"
 
   local widths = section_widths(data, sections)
+  local extra_padding = calculate_extra_padding(widths, sections) - 1
 
   -- Create headers and rows using modulo for dynamic wrapping
   local current_headers = {}
   local current_rows = {}
 
   for index, section in ipairs(sections) do
-    local formatted_section = pad_string(section, widths[section])
+    local formatted_section = pad_string(section, (widths[section] + extra_padding))
     table.insert(current_headers, formatted_section)
 
     local max_rows = #data[section] or 0
@@ -42,7 +57,8 @@ function M.pretty_print(data, sections)
     for row_index = 1, max_rows do
       local item = data[section][row_index]
       if item then
-        local formatted_item = pad_string(string.format("%s (%s)", item.name, item.value), widths[section] or 10)
+        local formatted_item =
+          pad_string(string.format("%s (%s)", item.name, item.value), (widths[section] + extra_padding) or 10)
         local current_value = current_rows[row_index] and current_rows[row_index].value or ""
 
         current_rows[row_index] = {
@@ -69,14 +85,14 @@ function M.pretty_print(data, sections)
     -- Add headers and rows to the layout when reaching max_cols or last section
     if index % max_cols == 0 or index == #sections then
       local header_row = table.concat(current_headers, pipe)
-      local divider_row = string.rep(dash, #header_row)
       table.insert(layout, header_row)
       table.insert(extmarks, { row = #layout - 1, start_col = 0, end_col = #header_row, hl_group = hl.symbols.header })
-      table.insert(layout, divider_row)
-      table.insert(
-        extmarks,
-        { row = #layout - 1, start_col = 0, end_col = #divider_row, hl_group = hl.symbols.success }
-      )
+      table.insert(extmarks, {
+        row = #layout - 1,
+        start_col = 0,
+        virt_text = { { string.rep(dash, #header_row + extra_padding), hl.symbols.success } },
+        virt_text_pos = "overlay",
+      })
 
       -- Insert rows
       for _, row in ipairs(current_rows) do
