a7e7ad88326ff88b074e4e5b7fdb982ef68697e3
Moshe Avni <mavni@netapp.com>
2024-09-18 16:18:48 +0300

    🙉 work in progress Wed Sep 18 16:18:48 2024

--- lua/kubectl/views/pods/definition.lua
+++ lua/kubectl/views/pods/definition.lua
@@ -38,7 +38,7 @@ local function getReady(row)
 end
 
 --- Get restarts as a symbol
----@param row table<table>
+---@param row table
 ---@param currentTime number
 ---@return table
 local function getRestarts(row, currentTime)
