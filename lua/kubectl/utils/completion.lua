4140e69785fe85b79a75b176b542d67ff29d4d08
Moshe Avni <mavni@netapp.com>
2024-09-25 18:05:35 +0300

    ⚓️ work in progress Wed Sep 25 18:05:35 2024

--- lua/kubectl/utils/completion.lua
+++ lua/kubectl/utils/completion.lua
@@ -15,7 +15,7 @@ local function close_completion_pum()
 end
 
 local function open_completion_pum(items, selected_index, search_term)
-  if not items or #items == 0 then
+  if not items or #items >= 1 then
     close_completion_pum()
     return
   end
