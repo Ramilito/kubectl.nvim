422091527289dcc193c16be26d0730777ce66513
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-09-26 15:04:29 +0200

    use cancellationtoken

--- lua/kubectl/views/overview/init.lua
+++ lua/kubectl/views/overview/init.lua
@@ -6,20 +6,20 @@ local url = require("kubectl.utils.url")
 
 local M = {}
 
-function M.View()
+function M.View(cancellationToken)
   local builder = ResourceBuilder:new(definition.resource)
   local cmds = {
     {
       cmd = "curl",
-      args = { "{{{BASE}}/apis/metrics.k8s.io/v1beta1/nodes?pretty=false}" },
+      args = { "{{BASE}}/apis/metrics.k8s.io/v1beta1/nodes?pretty=false" },
     },
     {
       cmd = "curl",
-      args = { "{{{BASE}}/api/v1/nodes?pretty=false}" },
+      args = { "{{BASE}}/api/v1/nodes?pretty=false" },
     },
     {
       cmd = "curl",
-      args = { "{{{BASE}}/apis/metrics.k8s.io/v1beta1/{{NAMESPACE}}pods?pretty=false}" },
+      args = { "{{BASE}}/apis/metrics.k8s.io/v1beta1/{{NAMESPACE}}pods?pretty=false" },
     },
     { cmd = "curl", args = { "{{BASE}}/api/v1/{{NAMESPACE}}pods?pretty=false" } },
     { cmd = "curl", args = { "{{BASE}}/apis/apps/v1/{{NAMESPACE}}replicasets?pretty=false" } },
@@ -33,14 +33,19 @@ function M.View()
     end
   end
 
-  commands.await_shell_command_async(cmds, function(data)
+  commands.await_shell_command_async(cmds, function(data, err)
     builder.data = data
     vim.schedule(function()
       builder:display(definition.ft, definition.resource)
       builder:decodeJson():process(definition.processRow, true)
 
       builder.prettyData, builder.extmarks = grid.pretty_print(builder.processedData, definition.getSections())
-      builder:addHints(definition.hints, true, true, true):setContent(nil)
+      builder:addHints(definition.hints, true, true, true)
+      if cancellationToken and cancellationToken() then
+        return nil
+      end
+
+      builder:setContent(nil)
     end)
   end)
 end
