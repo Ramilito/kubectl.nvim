04fffeacc46d92c61cfcd63089c9c18051647cc4
Moshe Avni <mavni@netapp.com>
2024-09-26 10:49:29 +0300

    fix/view-5

--- lua/kubectl/views/init.lua
+++ lua/kubectl/views/init.lua
@@ -73,7 +73,7 @@ function M.Hints(headers)
     { key = "<Plug>(kubectl.view_2)", desc = "Pods " },
     { key = "<Plug>(kubectl.view_3)", desc = "Configmaps " },
     { key = "<Plug>(kubectl.view_4)", desc = "Secrets " },
-    { key = "<Plug>(kubectl.view_4)", desc = "Services" },
+    { key = "<Plug>(kubectl.view_5)", desc = "Services" },
   }
 
   local global_keymaps = tables.get_plug_mappings(globals, "n")
