360a290f6fb7e63955ba0e4ff0a8c1667bca45a9
Moshe Avni <mavni@netapp.com>
2024-09-27 14:54:39 +0300

    fix/namespaced-on-fallback (#377)

--- lua/kubectl/utils/tables.lua
+++ lua/kubectl/utils/tables.lua
@@ -374,6 +374,15 @@ function M.getCurrentSelection(...)
   return unpack(results)
 end
 
+function M.find_index(haystack, needle)
+  for index, value in ipairs(haystack) do
+    if value == needle then
+      return index
+    end
+  end
+  return nil -- Return nil if the needle is not found
+end
+
 function M.find_resource(data, name, namespace)
   if data.items then
     return vim.iter(data.items):find(function(row)
@@ -383,7 +392,7 @@ function M.find_resource(data, name, namespace)
   if data.rows then
     return vim.iter(data.rows):find(function(row)
       return row.object.metadata.name == name and row.object.metadata.namespace == namespace
-    end)
+    end).object
   end
   return nil
 end
