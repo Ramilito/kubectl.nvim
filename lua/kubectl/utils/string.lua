2db5118ee81a1c8f167ec4deb9200df0abd73825
Moshe Avni <mavni@netapp.com>
2024-09-27 12:05:10 +0300

    feat/remove-strings-trim

--- lua/kubectl/utils/string.lua
+++ lua/kubectl/utils/string.lua
@@ -1,12 +1,5 @@
 local M = {}
 
---- Trim whitespace from the beginning and end of a string
----@param s string
----@return string
-function M.trim(s)
-  return s:match("^%s*(.-)%s*$")
-end
-
 function M.capitalize(str)
   return str:sub(1, 1):upper() .. str:sub(2)
 end
