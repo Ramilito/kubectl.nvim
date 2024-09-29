63418bfc855b5357b2c6f4f46632550be23a24b6
Moshe Avni <mavni@netapp.com>
2024-09-29 21:04:47 +0300

    feat/logger (#404)

--- lua/kubectl/utils/timeme.lua
+++ lua/kubectl/utils/timeme.lua
@@ -7,7 +7,8 @@ end
 
 M.stop = function()
   local elapsed_time = vim.fn.reltimefloat(vim.fn.reltime(M.startTime))
-  print("Execution time: " .. elapsed_time .. " seconds")
+  local log = require("kubectl.log")
+  log.fmt_info("Execution time: %.6f seconds", elapsed_time)
 end
 
 return M
