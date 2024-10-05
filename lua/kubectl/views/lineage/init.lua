26313c1d7b75898eba7dfd835b89cb36ba0434f0
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-10-05 21:50:03 +0200

    fix kind plurar => singular

--- lua/kubectl/views/lineage/init.lua
+++ lua/kubectl/views/lineage/init.lua
@@ -7,12 +7,18 @@ local M = {}
 function M.View(name, ns, kind)
   local data = definition.collect_all_resources(view.cached_api_resources.values)
   local graph = definition.build_graph(data)
-
-  local selected_key = string.lower(kind) .. "/" .. ns .. "/" .. name
+  if kind:sub(-1) == "s" then
+    kind = kind:sub(1, -2)
+  end
+  local selected_key = kind .. "/" .. ns .. "/" .. name
 
   local associated_resources = definition.find_associated_resources(graph, selected_key)
-  logger.notify_table(associated_resources)
 
+  print("Associated Resources:", selected_key)
+  for _, res in ipairs(associated_resources) do
+    local res_ns = res.ns or "cluster"
+    print("- " .. res.kind .. ": " .. res_ns .. "/" .. res.name)
+  end
 end
 
 return M
