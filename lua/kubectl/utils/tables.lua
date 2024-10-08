09f1f648af4e1db81626c1e19a31adf9c6fc708f
Moshe Avni <mavni@netapp.com>
2024-10-08 19:10:36 +0300

    better divider

--- lua/kubectl/utils/tables.lua
+++ lua/kubectl/utils/tables.lua
@@ -281,7 +281,7 @@ local function addDividerRow(divider, hints, marks)
     local count = divider.count or ""
     local filter = divider.filter or ""
     local info = resource .. count .. filter
-    local padding = string.rep("-", half_width - math.floor(#info / 2))
+    local padding = string.rep("—", half_width - math.floor(#info / 2))
 
     local virt_text = {
       { padding, hl.symbols.success },
@@ -305,7 +305,7 @@ local function addDividerRow(divider, hints, marks)
       virt_text_pos = "overlay",
     })
   else
-    local padding = string.rep("-", half_width)
+    local padding = string.rep("—", half_width)
     row = padding .. padding
     table.insert(marks, {
       row = #hints,
