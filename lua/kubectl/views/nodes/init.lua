800f00e6e078a47bafe395af6c76f9f69c34ee56
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-10-22 23:45:51 +0200

    prepare stuff

--- lua/kubectl/views/nodes/init.lua
+++ lua/kubectl/views/nodes/init.lua
@@ -5,7 +5,13 @@ local definition = require("kubectl.views.nodes.definition")
 local state = require("kubectl.state")
 local tables = require("kubectl.utils.tables")
 
-local M = {}
+local M = {
+  grace_period = "-1",
+  timeout = "5s",
+  ignore_daemonset = "false",
+  delete_local_data = "false",
+  force = "false",
+}
 
 function M.View(cancellationToken)
   ResourceBuilder:view(definition, cancellationToken)
@@ -16,7 +22,27 @@ function M.Draw(cancellationToken)
 end
 
 function M.Drain(node)
-  commands.shell_command_async("kubectl", { "drain", "nodes/" .. node })
+  local builder = ResourceBuilder:new("kubectl_drain")
+  local win_config
+
+  builder.buf_nr, win_config = buffers.confirmation_buffer("Drain node: " .. node .. "?", "", function(confirm)
+    if confirm then
+      -- commands.shell_command_async("kubectl", { "drain", "nodes/" .. node })
+    end
+  end)
+
+  builder.data = {}
+  local confirmation = "[y]es [n]o:"
+  local padding = string.rep(" ", (win_config.width - #confirmation) / 2)
+
+  table.insert(builder.data, "GracePeriod: " .. M.grace_period)
+  table.insert(builder.data, "Timeout: " .. M.timeout)
+  table.insert(builder.data, "Ignore Daemonset: " .. M.ignore_daemonset)
+  table.insert(builder.data, "Delete local data: " .. M.delete_local_data)
+  table.insert(builder.data, "Force: " .. M.force)
+  table.insert(builder.data, padding .. confirmation)
+
+  builder:setContentRaw()
 end
 
 function M.UnCordon(node)
