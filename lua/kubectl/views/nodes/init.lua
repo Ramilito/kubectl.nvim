24c5286b3663ad668d90ea1d4d3afbb4a18021ca
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-10-23 02:53:00 +0200

    reset state and give better feedback

--- lua/kubectl/views/nodes/init.lua
+++ lua/kubectl/views/nodes/init.lua
@@ -6,9 +6,7 @@ local hl = require("kubectl.actions.highlight")
 local state = require("kubectl.state")
 local tables = require("kubectl.utils.tables")
 
-local M = {
-  drain_args = {},
-}
+local M = {}
 
 function M.View(cancellationToken)
   ResourceBuilder:view(definition, cancellationToken)
@@ -38,7 +36,7 @@ local function add_row(builder, text, value)
 end
 
 function M.Drain(node)
-  M.drain_args = {}
+  local drain_args = {}
   local builder = ResourceBuilder:new("kubectl_drain")
   local win_config
 
@@ -47,52 +45,46 @@ function M.Drain(node)
     "k8s_node_drain",
     function(confirm)
       if confirm then
-        vim.print(M.drain_args)
-        -- commands.shell_command_async("kubectl", { "drain", "nodes/" .. node })
+        commands.shell_command_async("kubectl", M.drain_args)
       end
     end
   )
 
   vim.api.nvim_buf_attach(builder.buf_nr, false, {
-    on_lines = function(
-      _, -- use nil as first argument (since it is buffer handle)
-      buf_nr, -- buffer number
-      _, -- buffer changedtick
-      firstline, -- first line number of the change (0-indexed)
-      lastline, -- last line number of the change
-      new_lastline -- last line number after the change
-    )
-      if firstline ~= lastline or lastline ~= new_lastline then
-        local lines = vim.api.nvim_buf_get_lines(buf_nr, 0, -1, false)
-        local grace_period, timeout, ignore_daemonset, delete_local_data, force
+    on_lines = function(_, buf_nr, _, first, last_orig, last_new, byte_count)
+      if first == last_orig and last_orig == last_new and byte_count == 0 then
+        return
+      end
+      local lines = vim.api.nvim_buf_get_lines(buf_nr, 0, -1, false)
+      local grace_period, timeout, ignore_daemonset, delete_local_data, force
 
-        for _, line in ipairs(lines) do
-          if line:match("Grace period:") then
-            grace_period = line:match("Grace period:%s*(.*)")
-          elseif line:match("Timeout:") then
-            timeout = line:match("Timeout:%s*(.*)")
-          elseif line:match("Ignore daemonset:") then
-            ignore_daemonset = line:match("Ignore daemonset:%s*(.*)")
-          elseif line:match("Delete local data:") then
-            delete_local_data = line:match("Delete local data:%s*(.*)")
-          elseif line:match("Force:") then
-            force = line:match("Force:%s*(.*)")
-          end
+      for _, line in ipairs(lines) do
+        if line:match("Grace period:") then
+          grace_period = line:match("Grace period:%s*(.*)")
+        elseif line:match("Timeout:") then
+          timeout = line:match("Timeout:%s*(.*)")
+        elseif line:match("Ignore daemonset:") then
+          ignore_daemonset = line:match("Ignore daemonset:%s*(.*)")
+        elseif line:match("Delete local data:") then
+          delete_local_data = line:match("Delete local data:%s*(.*)")
+        elseif line:match("Force:") then
+          force = line:match("Force:%s*(.*)")
         end
+      end
 
-        local args = { "drain", "nodes/" .. node, "--grace-period", grace_period, "--timeout", timeout }
+      local args = { "drain", "nodes/" .. node, "--grace-period", grace_period, "--timeout", timeout }
 
-        if ignore_daemonset == "true" then
-          table.insert(args, "--ignore-daemonsets")
-        end
-        if delete_local_data == "true" then
-          table.insert(args, "--delete-local-data")
-        end
-        if force == "true" then
-          table.insert(args, "--force")
-        end
+      if ignore_daemonset == "true" then
+        table.insert(args, "--ignore-daemonsets")
+      end
+      if delete_local_data == "true" then
+        table.insert(args, "--delete-local-data")
+      end
+      if force == "true" then
+        table.insert(args, "--force")
+      end
 
-        M.drain_args = args
+      if table.concat(drain_args, " ") ~= table.concat(args, " ") then
         for i, line in ipairs(lines) do
           if line:match("^Args:") then
             vim.schedule(function()
@@ -102,6 +94,7 @@ function M.Drain(node)
           end
         end
       end
+      drain_args = args
     end,
   })
 
