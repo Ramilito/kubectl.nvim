ee94320de273275be21a8f8d8b21eaa8e5be4861
Moshe Avni <mavni@netapp.com>
2024-10-25 14:37:10 +0300

    remove unused import

--- lua/kubectl/views/jobs/init.lua
+++ lua/kubectl/views/jobs/init.lua
@@ -1,5 +1,4 @@
 local ResourceBuilder = require("kubectl.resourcebuilder")
-local commands = require("kubectl.actions.commands")
 local definition = require("kubectl.views.jobs.definition")
 local state = require("kubectl.state")
 local tables = require("kubectl.utils.tables")
