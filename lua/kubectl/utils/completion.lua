159cbdcd06a9205cf08c37606b229f5d916247b1
Moshe Avni <moshe@avni.us>
2025-01-09 11:11:37 +0200

    fix/completion-filter-on-nightly (#532)

--- lua/kubectl/utils/completion.lua
+++ lua/kubectl/utils/completion.lua
@@ -92,16 +92,14 @@ function M.with_completion(buf, data, callback, shortest)
   local current_suggestion_index = 0
 
   vim.api.nvim_buf_attach(buf, false, {
-    on_lines = function()
+    on_lines = vim.schedule_wrap(function()
       if config.options.completion.follow_cursor and M.pum_win and vim.api.nvim_win_is_valid(M.pum_win) then
-        vim.schedule(function()
-          vim.api.nvim_win_set_config(M.pum_win, {
-            relative = "cursor",
-            anchor = "NW",
-            row = 1,
-            col = 0,
-          })
-        end)
+        vim.api.nvim_win_set_config(M.pum_win, {
+          relative = "cursor",
+          anchor = "NW",
+          row = 1,
+          col = 0,
+        })
       end
       local line = vim.api.nvim_get_current_line()
       local input = line:sub(3) -- Remove the `% ` prefix to get the user input
@@ -110,7 +108,7 @@ function M.with_completion(buf, data, callback, shortest)
         current_suggestion_index = 0
         close_completion_pum(M.pum_win)
       end
-    end,
+    end),
     on_detach = function()
       close_completion_pum(M.pum_win)
       if M.ns then
