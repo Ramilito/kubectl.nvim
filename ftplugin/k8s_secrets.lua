2698f983c934e79db4c8417a1e085a611007aa2c
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-02-12 14:55:48 +0100

    migrate secrets and secret_desc

--- ftplugin/k8s_secrets.lua
+++ ftplugin/k8s_secrets.lua
@@ -1,12 +1,8 @@
 local loop = require("kubectl.utils.loop")
 local secrets_view = require("kubectl.views.secrets")
 
---- Set key mappings for the buffer
-local function set_keymaps(_) end
-
 --- Initialize the module
 local function init()
-  set_keymaps(0)
   if not loop.is_running() then
     loop.start_loop(secrets_view.Draw)
   end
