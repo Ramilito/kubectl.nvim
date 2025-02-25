c59b4596d1a22aaec73db38ee32d0c3255881b83
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-02-25 22:41:55 +0100

    fix loop

--- lua/kubectl/utils/loop.lua
+++ lua/kubectl/utils/loop.lua
@@ -6,7 +6,7 @@ local active_sessions = {}
 
 --- Start a loop for a specific buffer
 ---@param buf number
----@param callback fun(is_cancelled: fun(): boolean)
+---@param callback fun()
 ---@param opts? { interval: number }: The arguments for the loop.
 function M.start_loop_for_buffer(buf, callback, opts)
   if timers[buf] then
@@ -16,22 +16,14 @@ function M.start_loop_for_buffer(buf, callback, opts)
 
   local interval = opts.interval or config.options.auto_refresh.interval
   local timer = vim.uv.new_timer()
-  local session_id = vim.loop.hrtime()
-  active_sessions[buf] = session_id
   local running = false
-
-  local function is_cancelled()
-    return vim.api.nvim_get_current_buf() ~= buf or active_sessions[buf] ~= session_id
-  end
 
   timer:start(0, interval, function()
     if not running then
       running = true
 
       vim.schedule(function()
-        if vim.api.nvim_get_current_buf() == buf then
-          callback(is_cancelled)
-        end
+        callback()
 
         running = false
       end)
@@ -47,7 +39,7 @@ function M.start_loop_for_buffer(buf, callback, opts)
     end,
   })
 
-  vim.api.nvim_create_autocmd({ "BufLeave", "BufDelete" }, {
+  vim.api.nvim_create_autocmd({"BufHidden", "BufUnload", "BufDelete" }, {
     buffer = buf,
     callback = function()
       M.stop_loop(buf)
