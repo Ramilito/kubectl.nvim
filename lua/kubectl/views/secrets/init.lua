76a80a08dd2ae9a8ffa9620c338e254065e39401
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-04-08 11:58:38 +0200

    migrate secrets

--- lua/kubectl/views/secrets/init.lua
+++ lua/kubectl/views/secrets/init.lua
@@ -3,26 +3,55 @@ local definition = require("kubectl.views.secrets.definition")
 local state = require("kubectl.state")
 local tables = require("kubectl.utils.tables")
 
-local M = {}
+local resource = "secrets"
+local M = {
+  definition = {
+    resource = resource,
+    display_name = string.upper(resource),
+    ft = "k8s_" .. resource,
+    gvk = { g = "", v = "v1", k = "secret" },
+    informer = { enabled = true },
+    headers = {
+      "NAMESPACE",
+      "NAME",
+      "TYPE",
+      "DATA",
+      "AGE",
+    },
+    processRow = definition.processRow,
+  },
+}
 
 function M.View(cancellationToken)
-  ResourceBuilder:view(definition, cancellationToken)
+  ResourceBuilder:view(M.definition, cancellationToken)
 end
 
 function M.Draw(cancellationToken)
-  state.instance[definition.resource]:draw(definition, cancellationToken)
+  state.instance[M.definition.resource]:draw(M.definition, cancellationToken)
 end
 
 function M.Desc(name, ns, reload)
-  ResourceBuilder:view_float({
-    resource = "secrets | " .. name .. " | " .. ns,
+  local def = {
+    resource = M.definition.resource .. " | " .. name .. " | " .. ns,
     ft = "k8s_secret_desc",
     url = { "get", "secret", name, "-n", ns, "-o", "yaml" },
     syntax = "yaml",
+    cmd = "describe_async",
     hints = {
       { key = "<Plug>(kubectl.select)", desc = "base64decode" },
     },
-  }, { cmd = "kubectl", reload = reload })
+  }
+
+  ResourceBuilder:view_float(def, {
+    args = {
+      state.context["current-context"],
+      M.definition.resource,
+      ns,
+      name,
+      M.definition.gvk.g,
+    },
+    reload = reload,
+  })
 end
 
 --- Get current seletion for view
