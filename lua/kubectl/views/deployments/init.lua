74b71a9416be097d7c16541a58a616826a7a7580
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-04-19 17:54:55 +0200

    fix deployment set image

--- lua/kubectl/views/deployments/init.lua
+++ lua/kubectl/views/deployments/init.lua
@@ -57,7 +57,7 @@ function M.SetImage(name, ns)
       return
     end
     builder.data = data
-    builder:decodeJson()
+    builder.decodeJson()
 
     local container_images = {}
     if builder.data.spec.template.spec.containers then
@@ -86,7 +86,7 @@ function M.SetImage(name, ns)
 
       builder.data = {}
       table.insert(builder.data, " ")
-      builder:action_view(def, params, function(args)
+      builder.action_view(def, params, function(args)
         local image_spec = {}
         for _, container in ipairs(args) do
           table.insert(image_spec, {
