128046a92c6e5f6ace2eb0733454f1dda9069c4d
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-04-22 01:41:44 +0200

    switch to using tracer

--- lua/kubectl/client/rust/log.rs
+++ lua/kubectl/client/rust/log.rs
@@ -1,26 +1,26 @@
-use std::sync::OnceLock;
-use fern::Dispatch;
-use log::LevelFilter;
-use chrono::Local;
+use std::{fs::File, sync::OnceLock};
+use tracing::Level;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::fmt;
 
-static LOGGER: OnceLock<()> = OnceLock::new();
+static TRACER: OnceLock<()> = OnceLock::new();
+static LOG_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
 
 pub fn setup_logger(log_file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    LOGGER.get_or_init(|| {
-        Dispatch::new()
-            .format(|out, message, record| {
-                out.finish(format_args!(
-                    "[{}][{}][{}] {}",
-                    Local::now().format("%Y-%m-%d %H:%M:%S"),
-                    record.level(),
-                    record.target(),
-                    message
-                ))
-            })
-            .level(LevelFilter::Info)
-            .chain(fern::log_file(log_file_path).unwrap())
-            .apply()
-            .unwrap()
+    TRACER.get_or_init(|| {
+        let file = File::create(format!("{}/kubectl.log", log_file_path))
+            .expect("Failed to create log file");
+        let (non_blocking_writer, guard) = tracing_appender::non_blocking(file);
+
+        let subscriber = fmt::Subscriber::builder()
+            .with_line_number(true)
+            .with_max_level(Level::INFO)
+            .with_writer(non_blocking_writer)
+            .finish();
+
+        LOG_GUARD.set(guard).ok();
+        tracing::subscriber::set_global_default(subscriber).expect("Failed to set global default");
     });
+
     Ok(())
 }
