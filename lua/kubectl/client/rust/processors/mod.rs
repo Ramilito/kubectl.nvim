d80da516fb8341e57d20469a225465658ed009a2
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-05-03 23:44:45 +0200

    add pvc to rust

--- lua/kubectl/client/rust/processors/mod.rs
+++ lua/kubectl/client/rust/processors/mod.rs
@@ -9,19 +9,21 @@ pub mod deployment;
 pub mod fallback;
 pub mod pod;
 pub mod processor;
+pub mod persistentvolumeclaim;
 pub mod statefulset;
 pub mod storageclass;
 
 pub fn get_processors() -> HashMap<&'static str, Box<dyn Processor>> {
     let mut map: HashMap<&str, Box<dyn Processor>> = HashMap::new();
+    map.insert("clusterrolebinding", Box::new(clusterrolebinding::ClusterRoleBindingProcessor));
+    map.insert("configmap", Box::new(configmap::ConfigmapProcessor));
+    map.insert("customresourcedefinition", Box::new(customresourcedefinition::ClusterResourceDefinitionProcessor));
     map.insert("default", Box::new(default::DefaultProcessor));
-    map.insert("pod", Box::new(pod::PodProcessor));
     map.insert("deployment", Box::new(deployment::DeploymentProcessor));
+    map.insert("fallback", Box::new(fallback::FallbackProcessor));
+    map.insert("persistentvolumeclaim", Box::new(persistentvolumeclaim::PersistentVolumeClaimProcessor));
+    map.insert("pod", Box::new(pod::PodProcessor));
     map.insert("statefulset", Box::new(statefulset::StatefulsetProcessor));
     map.insert("storageclass", Box::new(storageclass::StorageClassProcessor));
-    map.insert("clusterrolebinding", Box::new(clusterrolebinding::ClusterRoleBindingProcessor));
-    map.insert("customresourcedefinition", Box::new(customresourcedefinition::ClusterResourceDefinitionProcessor));
-    map.insert("configmap", Box::new(configmap::ConfigmapProcessor));
-    map.insert("fallback", Box::new(fallback::FallbackProcessor));
     map
 }
--- lua/kubectl/views/persistentvolumeclaims/init.lua
+++ lua/kubectl/views/persistentvolumeclaims/init.lua
@@ -25,7 +25,7 @@ local M = {
       "STORAGE CLASS",
       "AGE",
     },
-    processRow = definition.processRow,
+    -- processRow = definition.processRow,
   },
 }
 
