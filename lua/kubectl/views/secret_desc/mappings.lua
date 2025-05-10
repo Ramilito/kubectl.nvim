7de2c3c036be0cef603d873dd0758dba1c535e52
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-05-10 12:18:44 +0200

    escape secret string

--- lua/kubectl/views/secret_desc/mappings.lua
+++ lua/kubectl/views/secret_desc/mappings.lua
@@ -7,15 +7,15 @@ M.overrides = {
     desc = "Base64Decode",
     callback = function()
       local line = vim.api.nvim_get_current_line()
-
       local code = line:match(":%s*(.+)")
+
       if code then
         local dec_ok, decoded = pcall(vim.base64.decode, code)
         if not dec_ok then
           vim.notify("Failed to decode base64: " .. decoded)
           return
         end
-        line = line:gsub(code, decoded)
+        line = line:gsub(vim.pesc(code), vim.pesc(decoded))
 
         local decoded_lines = vim.split(line, "\n", { plain = true, trimempty = true })
         local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
