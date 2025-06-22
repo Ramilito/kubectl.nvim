e34a8b196371c0093a770e20e1357b5f7edd5596
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-06-22 12:15:06 +0200

    fix configmap sorting on data

--- kubectl-client/src/processors/configmap.rs
+++ kubectl-client/src/processors/configmap.rs
@@ -4,14 +4,14 @@ use kube::api::DynamicObject;
 use mlua::prelude::*;
 
 use crate::processors::processor::Processor;
-use crate::utils::{AccessorMode, FieldValue};
+use crate::utils::{pad_key, AccessorMode, FieldValue};
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub struct ConfigmapProcessed {
     namespace: String,
     name: String,
     #[serde(rename = "data")]
-    binary_data: usize,
+    binary_data: FieldValue,
     age: FieldValue,
 }
 
@@ -25,10 +25,15 @@ impl Processor for ConfigmapProcessor {
         let map: ConfigMap =
             from_value(to_value(obj).expect("Failed to convert DynamicObject to JSON Value"))
                 .expect("Failed to convert JSON Value into ClusterRoleBinding");
+        let binary_data = map.data.as_ref().map_or(0, |map| map.len());
         Ok(ConfigmapProcessed {
             namespace: map.metadata.namespace.clone().unwrap_or_default(),
             name: map.metadata.name.clone().unwrap_or_default(),
-            binary_data: map.data.as_ref().map_or(0, |map| map.len()),
+            binary_data: FieldValue {
+                value: binary_data.to_string(),
+                sort_by: Some(binary_data),
+                ..Default::default()
+            },
             age: self.get_age(obj),
         })
     }
@@ -44,7 +49,10 @@ impl Processor for ConfigmapProcessor {
         Box::new(move |resource, field| match field {
             "namespace" => Some(resource.namespace.clone()),
             "name" => Some(resource.name.clone()),
-            "binary_data" => Some(resource.binary_data.clone().to_string()),
+            "data" => match mode {
+                AccessorMode::Sort => resource.binary_data.sort_by.map(pad_key),
+                AccessorMode::Filter => Some(resource.binary_data.value.clone()),
+            },
             "age" => match mode {
                 AccessorMode::Sort => Some(resource.age.sort_by?.to_string()),
                 AccessorMode::Filter => Some(resource.age.value.clone()),
