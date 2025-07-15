ae0bfd0ff67a6705ceb791b77bd2028a4f6c3481
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-07-15 23:14:00 +0200

    let refresh redo the watcher

--- kubectl-client/src/store.rs
+++ kubectl-client/src/store.rs
@@ -27,12 +27,6 @@ pub async fn init_reflector_for_kind(
     gvk: GroupVersionKind,
     namespace: Option<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    // {
-    //     let map = get_store_map().read().await;
-    //     if map.contains_key(&gvk.kind) {
-    //         return Ok(());
-    //     }
-    // }
     let ar = ApiResource::from_gvk(&gvk);
     let api: Api<DynamicObject> = match namespace {
         Some(ns) => Api::namespaced_with(client.clone(), &ns, &ar),
