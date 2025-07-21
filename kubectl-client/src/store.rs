630f134d0c196d5743a256d2bb45174f358a79ad
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-07-21 23:52:23 +0200

    fix: revert timer fix, use lower timeout

--- kubectl-client/src/store.rs
+++ kubectl-client/src/store.rs
@@ -13,29 +13,13 @@ use kube::runtime::reflector::Store;
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, OnceLock};
-use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::sync::RwLock;
-use tracing::info;
 
-pub struct StoreEntry {
-    reader: Store<DynamicObject>,
-    last_event: Arc<AtomicU64>,
-}
+type StoreMap = Arc<RwLock<HashMap<String, Store<DynamicObject>>>>;
+pub static STORE_MAP: OnceLock<StoreMap> = OnceLock::new();
 
-pub static STORE_MAP: OnceLock<Arc<RwLock<HashMap<String, StoreEntry>>>> = OnceLock::new();
-const STALE_AFTER_SECS: u64 = 25;
-
-pub fn get_store_map() -> Arc<RwLock<HashMap<String, StoreEntry>>> {
-    STORE_MAP
-        .get_or_init(|| Arc::new(RwLock::new(HashMap::new())))
-        .clone()
-}
-
-fn now() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
+pub fn get_store_map() -> &'static Arc<RwLock<HashMap<String, Store<DynamicObject>>>> {
+    STORE_MAP.get_or_init(|| Arc::new(RwLock::new(HashMap::new())))
 }
 
 #[tracing::instrument(skip(client))]
@@ -44,17 +28,10 @@ pub async fn init_reflector_for_kind(
     gvk: GroupVersionKind,
     namespace: Option<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let store_map = get_store_map();
     {
-        let map = store_map.read().await;
-        if let Some(e) = map.get(&gvk.kind) {
-            let healthy = now() - e.last_event.load(Ordering::Relaxed) <= STALE_AFTER_SECS;
-            if healthy {
-                info!("watcher is healthy");
-                return Ok(());
-            } else {
-                info!("resetting watcher");
-            }
+        let map = get_store_map().read().await;
+        if map.contains_key(&gvk.kind) {
+            return Ok(());
         }
     }
 
@@ -85,38 +62,26 @@ pub async fn init_reflector_for_kind(
         .default_backoff()
         .reflect(writer);
 
-    let last_event = Arc::new(AtomicU64::new(now()));
-    let le = last_event.clone();
     tokio::spawn(async move {
-        stream
-            .for_each(move |_res| {
-                le.store(now(), Ordering::Relaxed);
-                futures::future::ready(())
-            })
-            .await;
+        stream.for_each(|_| futures::future::ready(())).await;
     });
 
     let _ = reader.wait_until_ready().await;
-
-    {
-        let mut map = store_map.write().await;
-        map.insert(gvk.kind.clone(), StoreEntry { reader, last_event });
-    }
+    let mut map = get_store_map().write().await;
+    map.insert(gvk.kind.to_string(), reader);
 
     Ok(())
 }
 
 #[tracing::instrument]
 pub async fn get(kind: &str, namespace: Option<String>) -> Result<Vec<DynamicObject>, mlua::Error> {
-    let store_map = get_store_map();
-    let map = store_map.read().await;
+    let map = get_store_map().read().await;
     let store = match map.get(&kind.to_string()) {
         Some(store) => store,
         None => return Ok(Vec::new()),
     };
 
     let result: Vec<DynamicObject> = store
-        .reader
         .state()
         .par_iter()
         .filter(|arc_obj| {
@@ -141,15 +106,13 @@ pub async fn get_single(
     namespace: Option<String>,
     name: &str,
 ) -> Result<Option<DynamicObject>, mlua::Error> {
-    let store_map = get_store_map();
-    let map = store_map.read().await;
+    let map = get_store_map().read().await;
 
     let store = map
         .get(&kind.to_string())
         .ok_or_else(|| mlua::Error::RuntimeError("No store found for kind".into()))?;
 
     let result = store
-        .reader
         .state()
         .iter()
         .find(|arc_obj| {
--- lua/kubectl/client/init.lua
+++ lua/kubectl/client/init.lua
@@ -8,9 +8,9 @@ local client = {
 
 function client.set_implementation()
   client.implementation = require("kubectl_client")
+  client.implementation.init_logging(vim.fn.stdpath("log"))
   local ok = client.implementation.init_runtime(state.context["current-context"])
   if ok then
-    client.implementation.init_logging(vim.fn.stdpath("log"))
     client.implementation.init_metrics()
   end
   return ok
