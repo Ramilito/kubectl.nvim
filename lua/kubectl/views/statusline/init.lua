b755a1974dd14cd6eb064e2f6fa52fb915ddf4c3
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-07-28 08:08:42 +0200

    build statusline

--- lua/kubectl/views/statusline/init.lua
+++ lua/kubectl/views/statusline/init.lua
@@ -17,20 +17,31 @@ M.View = function()
 end
 
 M.Draw = function()
-  commands.run_async("get_statusline_async", {}, function(data, err)
-    if err then
-      return
-    end
-    print(data)
-
-		-- local ok, result = pcall(vim.api.nvim_set_option_value, "statusline", M.generateStatusline(), { scope = "global" })
-		-- print(ok, result)
-  end)
-
+  local builder = manager.get("statusline")
+  if builder then
+    commands.run_async("get_statusline_async", {}, function(data, err)
+      if err then
+        return
+      end
+      builder.data = data
+      builder.decodeJson()
+      vim.schedule(function()
+        pcall(vim.api.nvim_set_option_value, "statusline", M.process(builder.data), { scope = "global" })
+      end)
+    end)
+  end
 end
 
-function M.generateStatusline()
-  return "test"
+function M.process(data)
+  local ready = data.ready or 0
+  local not_ready = data.not_ready or 0
+  local total = ready + not_ready
+  local cpu = data.cpu_pct or 0
+  local mem = data.mem_pct or 0
+
+  local dot = (not_ready == 0) and "🟢" or "🔴"
+
+  return string.format("%s %d/%d │ CPU %d │ MEM %d", dot, ready, total, cpu, mem)
 end
 
 return M
