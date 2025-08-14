72ee3d5b085ed8f5134b8b08dcccec7fbf21c4de
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-08-14 21:54:04 +0200

    fix: fallback loop throttle fix

--- lua/kubectl/resources/fallback/init.lua
+++ lua/kubectl/resources/fallback/init.lua
@@ -93,6 +93,8 @@ function M.Draw(cancellationToken)
         builder.prettyPrint(win_id).addDivider(true).addHints(builder.definition.hints, true, true)
         builder.displayContent(win_id, cancellationToken)
       end
+      local loop = require("kubectl.utils.loop")
+      loop.set_running(builder.buf_nr, false)
     end)
   end)
 end
