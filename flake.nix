f4a63deaa86e0b98ce860dfa02dfd580e9ff9f85
Rami Daghlawi <rami.daghlawi@gmail.com>
2026-01-28 17:31:20 +0100

    some improvements

--- flake.nix
+++ flake.nix
@@ -4,19 +4,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
 
-        # Go static library (mirrors: make build_go)
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        # Go static library
         kubectl-go = pkgs.buildGoModule {
           pname = "kubectl-go";
           version = "0.1.0";
-          src = ./.;
-          modRoot = "go";
+          src = ./go;
           vendorHash = "sha256-lh5DAqr8o13q++5VnfJnVOcBpfKI6fSm+Qch15B/DRE=";
           buildPhase = ''
             runHook preBuild
@@ -31,47 +41,94 @@
           '';
         };
 
-        # Rust library (mirrors: make build)
-        kubectl-client = pkgs.rustPlatform.buildRustPackage {
+        # Source with only Rust files + Cargo
+        rustSrc = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = pkgs.lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            ./kubectl-client
+            ./kubectl-telemetry
+          ];
+        };
+
+        # Common args for crane builds
+        commonArgs = {
           pname = "kubectl-client";
           version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+          src = rustSrc;
+          strictDeps = true;
 
           nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          buildInputs = with pkgs; [
+            openssl
+            luajit
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            libiconv
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
           ];
 
+          # Link Go library
           preBuild = ''
             mkdir -p go
             ln -sf ${kubectl-go}/libkubectl_go.a go/
             ln -sf ${kubectl-go}/libkubectl_go.h go/
           '';
 
-          RUSTFLAGS = pkgs.lib.optionalString pkgs.stdenv.isDarwin
-            "-C link-arg=-undefined -C link-arg=dynamic_lookup";
-        };
-      in
-      {
-        packages = {
-          inherit kubectl-go kubectl-client;
+          # cdylib output goes to lib/
+          postInstall = ''
+            mkdir -p $out/lib
+            find target -name "libkubectl_client.so" -exec cp {} $out/lib/ \; 2>/dev/null || true
+            find target -name "libkubectl_client.dylib" -exec cp {} $out/lib/ \; 2>/dev/null || true
+          '';
 
-          default = pkgs.vimUtils.buildVimPlugin {
-            pname = "kubectl.nvim";
-            version = "main";
-            src = pkgs.lib.cleanSource ./.;
-            postPatch = ''
-              mkdir -p target/release
-              cp ${kubectl-client}/lib/libkubectl_client.* target/release/
-            '';
-            doCheck = false;
+          env = {
+            OPENSSL_NO_VENDOR = "1";
+          } // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
+            RUSTFLAGS = "-C link-arg=-undefined -C link-arg=dynamic_lookup";
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ go cargo rustc luacheck stylua ];
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Rust library
+        kubectl-client = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+
+        # Library extension for this platform
+        libExt = if pkgs.stdenv.isDarwin then "dylib" else "so";
+
+      in {
+        packages = {
+          inherit kubectl-go kubectl-client;
+
+          kubectl-nvim = pkgs.vimUtils.buildVimPlugin {
+            pname = "kubectl.nvim";
+            version = "main";
+            src = pkgs.lib.cleanSource ./.;
+            preInstall = ''
+              mkdir -p target/release
+              ln -s ${kubectl-client}/lib/libkubectl_client.${libExt} target/release/
+            '';
+            doCheck = false;
+          };
+
+          default = self.packages.${system}.kubectl-nvim;
+        };
+
+        apps.build-plugin = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellScriptBin "build-plugin" ''
+            mkdir -p target/release
+            cp -f ${kubectl-client}/lib/libkubectl_client.${libExt} target/release/
+            echo "Library copied to target/release/"
+          '';
+        };
+
+        devShells.default = craneLib.devShell {
+          inputsFrom = [ kubectl-client ];
+          packages = with pkgs; [ go luacheck stylua ];
         };
       }
     );
